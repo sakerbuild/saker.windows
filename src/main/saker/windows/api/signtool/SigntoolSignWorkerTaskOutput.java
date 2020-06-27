24912fc04d4fa197549b15bacdb694a0193b00f7
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-06-27 14:42:04 +0200

    Add javadoc

--- src/main/saker/windows/api/signtool/SigntoolSignWorkerTaskOutput.java
+++ src/main/saker/windows/api/signtool/SigntoolSignWorkerTaskOutput.java
@@ -2,6 +2,14 @@ package saker.windows.api.signtool;
 
 import saker.build.file.path.SakerPath;
 
+/**
+ * Output of the signtool signer task.
+ */
 public interface SigntoolSignWorkerTaskOutput {
+	/**
+	 * Gets the path to the signed file.
+	 * 
+	 * @return The absolute execution path.
+	 */
 	public SakerPath getPath();
 }
